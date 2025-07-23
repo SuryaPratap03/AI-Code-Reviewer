87cb3532013507b27052a446798967687ae814be
Surya Pratap Singh <sp7763360@gmail.com>
2025-07-23 23:27:25 +0530

    code refactored

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import app from './src/app.js'
 
-app.listen(process.env.PORT,()=>{
+const PORT=process.env.PORT || 8080
+app.listen(PORT,()=>{
     console.log(`Server is started on ${process.env.PORT}`)
 })
